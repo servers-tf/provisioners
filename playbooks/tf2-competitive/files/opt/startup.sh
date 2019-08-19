120dd3822000b99b4e297fc1655d292320ae0721
Patrick Hennessy <phennessy517@gmail.com>
2019-08-19 01:12:22 -0500

    Move Packer build to Amazon

--- playbooks/tf2-competitive/files/opt/startup.sh
+++ playbooks/tf2-competitive/files/opt/startup.sh
@@ -3,38 +3,47 @@
 SECRET_CONFIG="/usr/games/steam/tf2/tf/cfg/secrets.cfg"
 ATTRIBUTES_FILE="/opt/attributes.ini"
 
-ATTRIBUTES=$(curl --silent http://metadata.google.internal/0.1/meta-data/attributes/)
+AVAILABILITY_ZONE=`curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone`
+REGION=${AVAILABILITY_ZONE::-1}
+aws configure set region $REGION
 
-ZONE=$(curl --silent http://metadata.google.internal/0.1/meta-data/zone | grep -iEo 'us-(east|central|west)[0-9]')
-declare -A LOCATION
-LOCATION["us-east1"]="Atlanta"
-LOCATION["us-east4"]="Balitmore"
-LOCATION["us-central1"]="Omaha"
-LOCATION["us-west1"]="Portland"
+export INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
+export TAGS=$(aws ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" | jq -r ".Tags | from_entries")
 
 rm -f $SECRET_CONFIG
 rm -f $ATTRIBUTES_FILE
 
-for attr in $ATTRIBUTES; do
-    key=$(echo $attr | sed 's/-/_/g')
-    value=`curl --silent http://metadata.google.internal/0.1/meta-data/attributes/$attr`
-    
-    echo "$key=$value" >> $ATTRIBUTES_FILE
+echo $TAGS | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > $ATTRIBUTES_FILE
+for line in $(cat $ATTRIBUTES_FILE); do
+    key=`echo $line | cut -f1 -d'='`
+    value=`echo $line | cut -f2 -d'='`
     declare -r $key="$value"
+    echo "$key = $value"
 done
+
+ZONE=$(curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone | grep -iEo 'us-(east|west)-[0-9]')
+declare -A LOCATION
+LOCATION["us-east-1"]="US East"
+LOCATION["us-east-2"]="US Central"
+LOCATION["us-west-1"]="US West"
+LOCATION["us-west-2"]="US West"
 
 echo "boottime=$(date -d "$(who -b | awk '{print $4,$3}' | tr - / )" +%s)" >> $ATTRIBUTES_FILE
 
-echo "discord_api_key $discord_api_key" >> $SECRET_CONFIG
-echo "discord_channel_id $discord_channel_id" >> $SECRET_CONFIG
+SECRETS=$(aws ssm get-parameters --names "demostf_api_key" "logstf_api_key" "server_global_admins" "discord_api_key" | jq -r ".Parameters | from_entries")
 
-echo "logstf_apikey $logstf_api_key" >> $SECRET_CONFIG
-echo "sm_demostf_apikey $demostf_api_key" >> $SECRET_CONFIG
+echo "logstf_apikey" $(echo $SECRETS | jq -r '.logstf_api_key') >> $SECRET_CONFIG
+echo "sm_demostf_apikey" $(echo $SECRETS | jq -r '.demostf_api_key') >> $SECRET_CONFIG
+echo "discord_api_key" $(echo $SECRETS | jq -r '.discord_api_key') >> $SECRET_CONFIG
+
+echo "discord_message_id $discord_message_id" >> $SECRET_CONFIG
+echo "discord_channel_id $discord_channel_id" >> $SECRET_CONFIG
 echo "sv_password $server_password" >> $SECRET_CONFIG
 echo "tv_password $spectate_password" >> $SECRET_CONFIG
-
-echo "sp_location ${LOCATION[$ZONE]}" >> $SECRET_CONFIG
-echo "sp_hostname ${hostname^}" >> $SECRET_CONFIG
+echo "sp_location \"${LOCATION[$ZONE]}\"" >> $SECRET_CONFIG
+echo "sp_hostname \"${hostname^}\"" >> $SECRET_CONFIG
 
 chown steam:steam $ATTRIBUTES_FILE
 chattr -i $ATTRIBUTES_FILE
+
+hostnamectl set-hostname $hostname
