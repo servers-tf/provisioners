3e26a13f257b1040da1fa66615176993c3638757
Patrick Hennessy <phennessy517@gmail.com>
2019-09-18 01:38:56 -0500

    remove tick builder

--- playbooks/tick-elk/files/opt/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ATTRIBUTES="http://metadata.google.internal/0.1/meta-data/attributes"
-
-
-#DISCORD_API_TOKEN=$(curl $ATTRIBUTES/discord-api-token)
-PUBLIC_IP_ADDRESS=$(curl --silent http://metadata.google.internal/0.1/meta-data/network | grep -iEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v '^10')
-
